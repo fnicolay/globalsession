d4acb265c160270f03750f482128c67ceb6f2ad7
fnicolay <fnicolay@gmail.com>
2019-04-18 18:15:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,85 +5,15 @@ Object.size = function(obj) {
     }
     return size;
 };
-		
-
-var ipaddress = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port      = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-var http = require("http");
-
-var server= http.createServer(function(request, response) {
-
-        response.writeHead(200, {"Content-Type": "text/plain","Access-Control-Allow-Origin":"*"});
-	console.log("ENTROU");
-
-        process.on('uncaughtException', function(err) 
-        {
-	        response.end("Exception");
-		});
-		console.log(request.method);
-        try
-        {
-        if(request.method == "POST")
-        {
-                var url = request.url;
-                if(url == "/auth")
-                {
-
-                var body = '';
-                request.on('data', function(chunk)
-                {
-                              body += chunk.toString();
-                });
-
-            request.on('end', function () {
-                var params = JSON.parse(body);
-                console.log("Recived Params: "+JSON.stringify(params));
-                        var uuId = params.uuid;
-                        var accessToken = params.access_token;
-
-                        var msg = {'op':'authdone','accessToken':accessToken};
-                        if(clients[uuId] != undefined || clients[uuId] != null)
-                        {
-                                clients[uuId].send(JSON.stringify(msg),{mask:false});
-                                delete clients[uuId];
-                                response.end('{"status":"OK"}');
-
-                        }
-                        else
-                        {
-                                response.end('{"status":"NOK"}');
-                        }
-                        });
-                }
-                else
-                {
-                          response.end('{"status":"NOK"}');
-
-                }
-        }
-        else
-        {
-                 response.end("NOT Supported");
-        }
-
-        }
-        catch(e)
-        {
-                response.end("Exception");
-
-        }
+var ip = process.env.OPENSHIFT_NODEJS_IP;
+var port1      = 8080;
+var port2      = 8081;
 
 
-}).listen(port,ipaddress, function () {
-
-    console.log( "Listening on " + ipaddress + ", server_port " + port  );
-
-});
 
 var WebSocketServer = require('ws').Server
 var uuid = require('node-uuid');
-var wss = new WebSocketServer({ path:'/gencode',server:server,autoAcceptConnections: false});
+var wss = new WebSocketServer({ path:'/gencode',port: port1,server:ip });
 
 
 var clients = {};
@@ -104,5 +34,66 @@ wss.on('connection', function connection(ws) {
   });
 
 });
-                                             
-                        
+
+
+
+var http = require("http");
+
+http.createServer(function(request, response) {
+	
+	response.writeHead(200, {"Content-Type": "text/plain","Access-Control-Allow-Origin":"*"});
+	process.on('uncaughtException', function(err) {
+			 response.end("Exception");
+	});
+	if(request.method == "POST")
+	{
+		var url = request.url;
+		if(url == "/auth")
+		{
+		
+		var body = '';
+		request.on('data', function(chunk) 
+		{
+			      body += chunk.toString();
+		});
+
+	    request.on('end', function () {
+		var params = JSON.parse(body);
+		console.log("Recived Params: "+JSON.stringify(params));
+		 	var uuId = params.uuid;
+		 	var accessToken = params.access_token;
+		 	
+		 	var msg = {'op':'authdone','accessToken':accessToken};
+		 	if(clients[uuId] != undefined || clients[uuId] != null)
+		 	{
+		 		console.log("Before "+Object.size(clients));
+			 	clients[uuId].send(JSON.stringify(msg),{mask:false});
+			 	delete clients[uuId];
+			 	console.log("After "+Object.size(clients));
+			 	
+			 	response.end('{"status":"OK"}');
+			
+		 	}
+		 	else
+		 	{
+		 		response.end('{"status":"NOK"}');
+		 	}
+
+		 	});
+		}
+		else
+		{
+			  response.end('{"status":"NOK"}');
+				 	
+		}
+	}
+	else
+	{
+		 response.end("NOT Supported");
+	}
+
+}).listen(port2,ip, function () {
+
+    console.log( "Listening on " + ipaddress + ", server_port " + port  );
+
+});
